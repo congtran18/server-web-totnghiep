c2b916797afda53b17f6646a954434d09ff78103
congtran18 <tranvanthanhcooong@gmail.com>
2022-06-11 00:33:18 +0700

    update code

--- src/collections/stripe/stripe.service.ts
+++ src/collections/stripe/stripe.service.ts
@@ -268,7 +268,7 @@ export class StripeService {
                 console.log("books nek", books)
                 console.log("books nek2", customer?.email)
                 // if (customer?.email) {
-                    await this.mailService.sendUserConfirmation(customer?.email || '', "Sách", createAt, total_details?.amount_shipping ? total_details?.amount_shipping : 0, expanded_session.amount_total || 0, books)
+                    await this.mailService.sendUserConfirmation("tranvanthanhcooong@gmail.com", "sách")
                 // }
                 return data;
             }
