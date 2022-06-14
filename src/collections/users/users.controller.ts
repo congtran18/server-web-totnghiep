59d87c63c1a8a97a70b3f16c44f2fb5e9d6c2046
congtran18 <tranvanthanhcooong@gmail.com>
2022-06-14 17:19:37 +0700

    update code

--- src/collections/users/users.controller.ts
+++ src/collections/users/users.controller.ts
@@ -289,7 +289,7 @@ export class UsersController {
   })
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Update User minutes and days' })
-  @Get('/update-time')
+  @Get('/update/update-time')
   @UseGuards(JwtAuthGuard)
   async updateTimeUser(@AuthJwt() payload: JwtPayload, @Body() UpdateUserTimesDto: UpdateUserTimesDto): Promise<BaseResponse<User | null>> {
     const response: BaseResponse<User | null> = {}
